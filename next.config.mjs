4be804302b786918d699a4e7fb107a0bf3c23181
Romain Bourdain <romain.bourdain93@gmail.com>
2024-06-23 12:16:32 +0200

    reconfiguring nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
   basePath: "/test-pages",
   reactStrictMode: true,
-  images: {
-    loader: "akamai",
-    path: "",
-  },
   output: "export",
 };
 
